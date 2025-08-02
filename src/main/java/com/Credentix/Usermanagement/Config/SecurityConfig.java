15718ea30ff232a716e480dd41491e97a108948f
luminous44 <aklemasultanakhi@gmail.com>
2025-08-02 10:59:37 +0600

    added security config #2

---
+++ src/main/java/com/Credentix/Usermanagement/Config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.Credentix.Usermanagement.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/**").permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/signIn")
+                        .loginProcessingUrl("/login")
+                        .permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
