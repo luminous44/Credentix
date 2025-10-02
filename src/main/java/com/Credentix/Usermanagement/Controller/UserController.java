cce6bc984405b12a801b5a904756a50f4fe19c27
luminous44 <aklemasultanakhi@gmail.com>
2025-10-02 16:17:32 +0600

    fixed bug of controller

--- src/main/java/com/Credentix/Usermanagement/Controller/UserController.java
+++ src/main/java/com/Credentix/Usermanagement/Controller/UserController.java
@@ -6,6 +6,7 @@ import com.Credentix.Usermanagement.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.security.Principal;
 
 @Controller
+@ControllerAdvice
 @RequestMapping("/user")
 public class UserController {
      @Autowired
